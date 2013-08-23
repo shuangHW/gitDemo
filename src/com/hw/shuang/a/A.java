4b75f32f0571481b914d1c0d1b261b6222709c24
shuangHW <alllove1205mine@sina.com>
2013-08-23 19:05:49 +0800

    Update A.java
    change on github.com

--- src/com/hw/shuang/a/A.java
+++ src/com/hw/shuang/a/A.java
@@ -1,5 +1,8 @@
 package com.hw.shuang.a;
 
 public class A {
-
+  public static void main(String [] args){
+    System.out.println("this is my first to use gitHub");
+    System.out.println("3Q");
+  }
 }
